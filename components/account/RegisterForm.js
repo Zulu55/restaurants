bb8d7592a205d59a4a62c5b20f5656364b0eb4b0
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-02-20 09:52:33 -0500

    Validate form

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,21 +1,77 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Icon, Input } from 'react-native-elements'
+import { size } from 'lodash'
+
+import { validateEmail } from '../../utils/helpers'
 
 export default function RegisterForm() {
     const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+    const [errorEmail, setErrorEmail] = useState("")
+    const [errorPassword, setErrorPassword] = useState("")
+    const [errorConfirm, setErrorConfirm] = useState("")
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text })
+    }
+
+    const registerUser = () => {
+        if (!validateData()) {
+            return;
+        }
+
+        console.log("Fuck yeahh!!!")
+    }
+
+    const validateData = () => {
+        setErrorConfirm("")
+        setErrorEmail("")
+        setErrorPassword("")
+        let isValid = true
+
+        if(!validateEmail(formData.email)) {
+            setErrorEmail("Debes de ingresar un email válido.")
+            isValid = false
+        }
+
+        if(size(formData.password) < 6) {
+            setErrorPassword("Debes ingresar una contraseña de al menos seis carácteres.")
+            isValid = false
+        }
+
+        if(size(formData.confirm) < 6) {
+            setErrorConfirm("Debes ingresar una confirmación de contraseña de al menos seis carácteres.")
+            isValid = false
+        }
+
+        if(formData.password !== formData.confirm) {
+            setErrorPassword("La contraseña y la confirmación no son iguales.")
+            setErrorConfirm("La contraseña y la confirmación no son iguales.")
+            isValid = false
+        }
+
+        return isValid
+    }
 
     return (
         <View style={styles.form}>
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu email..."
+                onChange={(e) => onChange(e, "email")}
+                keyboardType="email-address"
+                errorMessage={errorEmail}
+                defaultValue={formData.email}
             />
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "password")}
+                errorMessage={errorPassword}
+                defaultValue={formData.password}
                 rightIcon={
                     <Icon
                         type="material-community"
@@ -30,6 +86,9 @@ export default function RegisterForm() {
                 placeholder="Confirma tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) => onChange(e, "confirm")}
+                errorMessage={errorConfirm}
+                defaultValue={formData.confirm}
                 rightIcon={
                     <Icon
                         type="material-community"
@@ -43,9 +102,14 @@ export default function RegisterForm() {
                 title="Registrar Nuevo Usuario"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
-                />
+                onPress={() => registerUser()}
+            />
         </View>
     )
+}
+
+const defaultFormValues = () => {
+    return { email: "", password: "", confirm: "" }
 }
 
 const styles = StyleSheet.create({
