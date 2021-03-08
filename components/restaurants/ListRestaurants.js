60ce18af4f52941af69b06c56f73f5e3bff392c1
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-03-08 16:17:20 -0500

    Show restaurants list

---
+++ components/restaurants/ListRestaurants.js
@@ -0,0 +1,76 @@
+import { size } from 'lodash'
+import React from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Image } from 'react-native-elements'
+
+export default function ListRestaurants({ restaurants, navigation }) {
+    return (
+        <View>
+            <FlatList
+                data={restaurants}
+                keyExtractor={(item, index) => index.toString() }
+                renderItem={(restaurant) => (
+                    <Restaurant restaurant={restaurant} navigation={navigation}/>
+                )}
+            />
+        </View>
+    )
+}
+
+function Restaurant({ restaurant, navigation }) {
+    const { id, images, name, address, description, phone, callingCode } = restaurant.item
+    const imageRestaurant = images[0]
+
+    return (
+        <TouchableOpacity>
+            <View style={styles.viewRestaurant}>
+                <View style={styles.viewRestaurantImage}>
+                    <Image
+                        resizeMode="cover"
+                        PlaceholderContent={<ActivityIndicator color="#fff"/>}
+                        source={{ uri: imageRestaurant }}
+                        style={styles.imageRestaurant}
+                    />
+                </View>
+                <View>
+                    <Text style={styles.restaurantTitle}>{name}</Text>
+                    <Text style={styles.restaurantInformation}>{address}</Text>
+                    <Text style={styles.restaurantInformation}>+{callingCode}-{phone}</Text>
+                    <Text style={styles.restaurantDescription}>
+                        {
+                            size(description) > 0
+                                ? `${description.substr(0, 60)}...`
+                                : description
+                        }
+                    </Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewRestaurant: {
+        flexDirection: "row",
+        margin: 10
+    },
+    viewRestaurantImage: {
+        marginRight: 15
+    },
+    imageRestaurant: {
+        width: 90,
+        height: 90
+    },
+    restaurantTitle: {
+        fontWeight: "bold"
+    },
+    restaurantInformation: {
+        paddingTop: 2,
+        color: "grey"
+    },
+    restaurantDescription: {
+        paddingTop: 2,
+        color: "grey",
+        width: "75%"
+    }
+})
--- screens/restaurants/Restaurants.js
+++ screens/restaurants/Restaurants.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Icon } from 'react-native-elements'
 import { useFocusEffect } from '@react-navigation/native'
+import { size } from 'lodash'
 import firebase from 'firebase/app'
 
 import Loading from '../../components/Loading'
+import ListRestaurants from '../../components/restaurants/ListRestaurants'
 import { getRestaurants } from '../../utils/actions'
 
 export default function Restaurants({ navigation }) {
@@ -14,7 +16,6 @@ export default function Restaurants({ navigation }) {
     const [loading, setLoading] = useState(false)
 
     const limitRestaurants = 7
-    console.log("restaurants", restaurants)
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged((userInfo) => {
@@ -41,7 +42,18 @@ export default function Restaurants({ navigation }) {
 
     return (
         <View style={styles.viewBody}>
-            <Text>Restaurants...</Text>
+            {
+                size(restaurants) > 0 ? (
+                    <ListRestaurants
+                        restaurants={restaurants}
+                        navigation={navigation}
+                    />
+                ) : (
+                    <View style={styles.notFoundView}>
+                        <Text style={styles.notFoundText}>No hay restaurantes registrados.</Text>
+                    </View>
+                )
+            }
             {
                 user && (
                     <Icon
@@ -70,5 +82,14 @@ const styles = StyleSheet.create({
         shadowColor: "black",
         shadowOffset: { width: 2, height: 2},
         shadowOpacity: 0.5
+    },
+    notFoundView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    notFoundText: {
+        fontSize: 18,
+        fontWeight: "bold"
     }
 })
