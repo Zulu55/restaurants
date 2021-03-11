d37e86384597d10b47886b96d5563fad8bc9826d
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-03-11 12:36:15 -0500

    Before fix problem in review

--- components/restaurants/ListReviews.js
+++ components/restaurants/ListReviews.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import firebase from 'firebase/app'
-import { Button } from 'react-native-elements'
-import Restaurant from '../../screens/restaurants/Restaurant'
+import { Avatar, Button, Rating } from 'react-native-elements'
+import moment from 'moment/min/moment-with-locales'
+import { getRestaurantReviews } from '../../utils/actions'
+import { size } from 'lodash'
+
+moment.locale("es")
 
 export default function ListReviews({ navigation, idRestaurant }) {
     const [userLogged, setUserLogged] = useState(false)
+    const [reviews, setReviews] = useState([])
 
     firebase.auth().onAuthStateChanged((user) => {
         user ? setUserLogged(true) : setUserLogged(false)
     })
+
+    useEffect(() => {
+        (async() => {
+            const response = await getRestaurantReviews(idRestaurant)
+            if (response.statusResponse) {
+                setReviews(response.reviews)
+            }
+        })()
+    }, [])
 
     return (
         <View>
@@ -38,6 +52,46 @@ export default function ListReviews({ navigation, idRestaurant }) {
                     </Text>
                 )
             }
+            {
+                size(reviews) > 0 && (
+                    map(reviews, (review, index) => (
+                        <Review key={index} review={review}/>
+                    ))
+                )
+            }
+        </View>
+    )
+}
+
+function Review({ key, review }) {
+    const { title, review, createAt, avatarUser, rating } = review 
+    const createReview = new Date(createAt.seconds * 10000)
+
+    return (
+        <View style={styles.viewReview}>
+            <View style={styles.imageAvatar}>
+                <Avatar
+                    renderPlaceholderContent={<ActivityIndicator color="#fff"/>}
+                    size="large"
+                    rounded
+                    containerStyle={styles.imageAvatarUser}
+                    source={
+                        avatarUser
+                            ? { uri: avatarUser}
+                            : require("../../assets/avatar-default.jpg")
+                    }
+                />
+            </View>
+            <View style={styles.viewInfo}>
+                <Text style={styles.reviewTitle}>{title}</Text>
+                <Text style={styles.reviewText}>{review}</Text>
+                <Rating
+                    imageSize={15}
+                    startingValue={rating}
+                    readonly
+                />
+                <Text style={styles.reviewDate}>{moment(createReview).format("LLL")}</Text>
+            </View>
         </View>
     )
 }
@@ -56,5 +110,39 @@ const styles = StyleSheet.create({
     },
     loginText: {
         fontWeight: "bold"
+    },
+    viewReview: {
+        flexDirection: "row",
+        padding: 10,
+        paddingBottom: 20,
+        borderBottomColor: "#e3e3e3",
+        borderBottomWidth: 1
+    },
+    imageAvatar: {
+        marginRight: 15
+    },
+    imageAvatarUser: {
+        width: 50,
+        height: 50
+    },
+    viewInfo: {
+        flex: 1,
+        alignItems: "flex-start"
+    },
+    reviewTitle: {
+        fontWeight: "bold"
+    },
+    reviewText: {
+        paddingTop: 2,
+        color: "gray",
+        marginBottom: 5
+    },
+    reviewDate: {
+        marginTop: 5,
+        color: "gray",
+        fontSize: 12,
+        position: "absolute",
+        right: 0,
+        bottom: 0
     }
 })
--- utils/actions.js
+++ utils/actions.js
@@ -188,3 +188,23 @@ export const updateDocument = async(collection, id, data) => {
     }
     return result     
 }
+
+export const getRestaurantReviews = async(id) => {
+    const result = { statusResponse: true, error: null, reviews: [] }
+    try {
+        const response = await db
+            .collection("reviews")
+            .orderBy("createAt", "desc")
+            .where("idRestaurant", "==", id)
+            .get()
+        response.forEach((doc) => {
+            const review = doc.data()
+            review.id = doc.id
+            result.reviews.push(review)
+        })
+    } catch (error) {
+        result.statusResponse = false
+        result.error = error
+    }
+    return result
+}
