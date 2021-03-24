0f64b3d458fdf96221d17d36ecafcd61e4f6bd47
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2021-03-24 16:36:59 -0500

    Send WA

--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -12,7 +12,7 @@ import Loading from '../../components/Loading'
 import MapRestaurant from '../../components/restaurants/MapRestaurant'
 import ListReviews from '../../components/restaurants/ListReviews'
 import { addDocumentWithoutId, getCurrentUser, getDocumentById, getIsFavorite, deleteFavorite } from '../../utils/actions'
-import { formatPhone } from '../../utils/helpers'
+import { callNumber, formatPhone, sendEmail, sendWhatsApp } from '../../utils/helpers'
 
 const widthScreen = Dimensions.get("window").width
 
@@ -24,10 +24,12 @@ export default function Restaurant({ navigation, route }) {
     const [activeSlide, setActiveSlide] = useState(0)
     const [isFavorite, setIsFavorite] = useState(false)
     const [userLogged, setUserLogged] = useState(false)
+    const [currentUser, setCurrentUser] = useState(null)
     const [loading, setLoading] = useState(false)
 
     firebase.auth().onAuthStateChanged(user => {
         user ? setUserLogged(true) : setUserLogged(false)
+        setCurrentUser(user)
     })
 
     navigation.setOptions({ title: name })
@@ -122,6 +124,9 @@ export default function Restaurant({ navigation, route }) {
                 address={restaurant.address}
                 email={restaurant.email}
                 phone={formatPhone(restaurant.callingCode, restaurant.phone)}
+                currentUser={currentUser}
+                callingCode={restaurant.callingCode}
+                phoneNoFormat={restaurant.phone}
             />
             <ListReviews
                 navigation={navigation}
@@ -133,12 +138,34 @@ export default function Restaurant({ navigation, route }) {
     )
 }
 
-function RestaurantInfo({ name, location, address, email, phone }) {
+function RestaurantInfo({ name, location, address, email, phone, currentUser, callingCode, phoneNoFormat}) {
     const listInfo = [
-        { text: address, iconName: "map-marker"},
-        { text: phone, iconName: "phone"},
-        { text: email, iconName: "at"},
+        { type: "addres", text: address, iconLeft: "map-marker" },
+        { type: "phone", text: phone, iconLeft: "phone", iconRight: "whatsapp" },
+        { type: "email", text: email, iconLeft: "at" },
     ]
+
+    const actionLeft = (type) => {
+        if (type == "phone") {
+            callNumber(phone)
+        } else if (type == "email") {
+            if (currentUser) {
+                sendEmail(email, "Interesado", `Soy ${currentUser.displayName}, estoy interesado en sus servicios`)
+            } else {
+                sendEmail(email, "Interesado", `Estoy interesado en sus servicios`)
+            }
+        }
+    }
+
+    const actionRight = (type) => {
+        if (type == "phone") {
+            if (currentUser) {
+                sendWhatsApp(`${callingCode} ${phoneNoFormat}`, `Soy ${currentUser.displayName}, estoy interesado en sus servicios`)
+            } else {
+                sendWhatsApp(`${callingCode} ${phoneNoFormat}`, `Estoy interesado en sus servicios`)
+            }
+        }
+    }
 
     return (
         <View style={styles.viewRestaurantInfo}>
@@ -158,12 +185,23 @@ function RestaurantInfo({ name, location, address, email, phone }) {
                     >
                         <Icon
                             type="material-community"
-                            name={item.iconName}
+                            name={item.iconLeft}
                             color="#442484"
+                            onPress={() => actionLeft(item.type)}
                         />
                         <ListItem.Content>
                             <ListItem.Title>{item.text}</ListItem.Title>
                         </ListItem.Content>
+                        {
+                            item.iconRight && (
+                                <Icon
+                                    type="material-community"
+                                    name={item.iconRight}
+                                    color="#442484"
+                                    onPress={() => actionRight(item.type)}
+                                />
+                            )
+                        }
                     </ListItem>
                 ))
             }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,8 @@
 import * as Permissions from 'expo-permissions'
 import * as ImagePicker from 'expo-image-picker'
 import * as Location from 'expo-location'
-import { Alert } from 'react-native'
+import { Alert, Linking } from 'react-native'
+import { size } from 'lodash'
 
 export function validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -53,6 +54,28 @@ export const getCurrentLocation = async() => {
 }
 
 export const formatPhone = (callingCode, phone) => {
+    if (size(phone) < 10)
+    {
+        return `+(${callingCode}) ${phone}`
+    }
     return `+(${callingCode}) ${phone.substr(0, 3)} ${phone.substr(3, 3)} ${phone.substr(6, 4)}`
-   }
-   
+}
+
+export const callNumber = (phoneNumber) => {
+    Linking.openURL(`tel:${phoneNumber}`)
+}
+
+export const sendWhatsApp = (phoneNumber, text) => {
+    const link = `https://wa.me/${phoneNumber}?text=${text}`
+    Linking.canOpenURL(link).then((supported) => {
+        if (!supported) {
+            Alert.alert("Por favor instale WhatsApp para enviar un mensaje directo")
+            return
+        }
+        return Linking.openURL(link)
+    })
+}
+
+export const sendEmail = (to, subject, body) => {
+    Linking.openURL(`mailto:${to}?subject=${subject}&body=${body}`)
+}   
